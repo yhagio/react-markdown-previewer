ea7fb38c49fdf432fc6334129900fdd23c1374f6
Yuichi Hagio <yhagio87@gmail.com>
2016-01-24 14:51:40 -0500

    Simple markdown

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple React Markdown Previewer
---
+++ components/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import marked from 'marked';
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      markdown: '# Hola'
+    };
+    this.convertMarkdown = this.convertMarkdown.bind(this);
+  }
+
+  convertMarkdown(e) {
+    this.setState({markdown: e.target.value});
+  }
+
+  render(){
+    var parsedMarkdown = marked(this.state.markdown);
+    return (
+      <div className="container">
+        <textarea
+          onChange={this.convertMarkdown}
+          className="textBox"
+          defaultValue="# Hola"
+          autoFocus></textarea>
+        <div 
+          dangerouslySetInnerHTML={{__html: parsedMarkdown}}
+           className="resultBox"/>
+      </div>
+    )
+  }
+};
+
+render(<App/>, document.getElementById('main'));
